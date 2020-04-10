51da851caa5a14bf061244401b27bbf28e2bcad2
CormacKrum <jgoel92@gmail.com>
2020-04-10 20:46:55 +0530

    Third Commit

--- app/src/main/java/com/example/healthcare/Profile.kt
+++ app/src/main/java/com/example/healthcare/Profile.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 
 /**
  * A simple [Fragment] subclass.
@@ -12,6 +13,9 @@ import android.view.ViewGroup
 class Profile : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_YES
+        )
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
