d10fe06f51746e7d4b11b509b368cd04a471632e
KUSHAL <kushalgera1212@gamil.com>
2020-04-11 11:03:35 +0530

    stats rec view created

--- app/src/main/java/com/example/healthcare/fragments/Analysis.kt
+++ app/src/main/java/com/example/healthcare/fragments/Analysis.kt
@@ -1,18 +1,19 @@
 package com.example.healthcare.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.healthcare.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class Analysis : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_analysis, container, false)
     }
 
